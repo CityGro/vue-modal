829c5d19827dcf6d344659a51c5fb4ecf52e1b65
Carlos Killpack <carlos@citygro.com>
2017-02-08 16:27:35 -0700

    implement promise-based interactions, close modals in order onESC, include bootstrap markup/classes

--- es/index.js
+++ es/index.js
@@ -1,31 +1,89 @@
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 import map from 'lodash/fp/map';
+import keyBy from 'lodash/fp/keyBy';
+import last from 'lodash/fp/last';
 import hash from 'object-hash';
+import findIndex from 'lodash/fp/findIndex';
 import values from 'lodash/fp/values';
 import EventEmmitter from 'events';
 import $ from 'jquery';
+import Q from 'q';
 
 export default {
   install: function install(Vue, options) {
     var modals = new EventEmmitter();
+    var stack = [];
     /**
      * style this component! use `.modal`, `.modal-container`, and `.modal-wrapper`
      * @function
      */
     var ModalWrapper = Vue.component('modal-wrapper', {
-      // eslint-disable-line no-unused-vars
       render: function render(h) {
-        return h('div', { class: { modal: true } }, [h('div', { class: { 'modal-container': true } }, [h('div', { class: { 'modal-wrapper': true } }, [this.$slots.default])])]);
+        return h('div', {
+          class: {
+            modal: true
+          }
+        }, [h('div', {
+          class: {
+            'modal-dialog': true
+          }
+        }, [h('div', {
+          class: {
+            'modal-content': true
+          }
+        }, [h('div', {
+          class: {
+            'modal-header': true
+          }
+        }, [h('button', {
+          class: { 'close': true },
+          attrs: {
+            'type': 'button',
+            'aria-label': 'Close'
+          }
+        }, [h('span', {
+          attrs: {
+            'aria-hidden': true
+          },
+          on: {
+            click: this.dismiss()
+          }
+        }, '×')]), h('h4', { class: { 'modal-title': true } }, this.title)]), h('div', { class: { 'modal-body': true } }, [this.$slots.default]), h('div', { class: { 'modal-footer': true } }, [h('button', {
+          class: {
+            'btn': true,
+            'btn-default': true
+          },
+          on: {
+            click: this.dismiss()
+          }
+        }, 'Close'), h('button', {
+          class: {
+            'btn': true,
+            'btn-primary': true
+          },
+          on: {
+            click: this.close()
+          }
+        }, 'OK')])])])]);
       },
 
       props: {
         id: {
           type: String,
           required: true
+        },
+        title: {
+          type: String,
+          required: true
         }
       },
       methods: {
         close: function close() {
-          modals.emit('destroy', this.id);
+          modals.emit('close', this.id);
+        },
+        dismiss: function dismiss() {
+          modals.emit('dismiss', this.id);
         }
       },
       mounted: function mounted() {
@@ -33,20 +91,13 @@ export default {
 
         var input = $(this.$el).find('.modal-wrapper').first();
         var onClick = function onClick(event) {
-          if (!input.is(event.target) && input.has(event.target).length === 0 && _this.showDropdown) {
+          if (!input.is(event.target) && input.has(event.target).length === 0) {
             _this.close();
           }
         };
         $(document).on('click', onClick);
-        var onKeydown = function onKeydown(event) {
-          if (_this.showDropdown && event.keyCode === 27) {
-            _this.close();
-          }
-        };
-        $(document).on('keydown', onKeydown);
         this._unsubscribe = function () {
           $(document).off('click', onClick);
-          $(document).off('keydown', onKeydown);
         };
       },
       beforeDestroy: function beforeDestroy() {
@@ -62,34 +113,67 @@ export default {
         var modals = map(function (_ref) {
           var Modal = _ref.Modal,
               data = _ref.data,
-              id = _ref.id;
+              id = _ref.id,
+              title = _ref.title;
           return h(ModalWrapper, {
-            attrs: { id: id }
+            attrs: { id: id },
+            props: { title: title }
           }, [h(Modal, { props: data })]);
         });
         return h('div', null, modals(values(this.modals)));
       },
-      data: function data() {
-        return { modals: {} };
-      },
 
+      computed: {
+        modals: function modals() {
+          return keyBy(function (_ref2) {
+            var _ref3 = _slicedToArray(_ref2, 1),
+                id = _ref3[0];
+
+            return id;
+          })(stack);
+        }
+      },
       /**
        * register listeners to add/remove Modals and corresponding data
        */
       beforeCreate: function beforeCreate() {
         var _this2 = this;
 
-        var onOpen = function onOpen(event) {
-          Vue.set(_this2.modals, event.id, event);
+        var onDestroy = function onDestroy(method) {
+          return function (id) {
+            var index = findIndex(function (_ref4) {
+              var _ref5 = _slicedToArray(_ref4, 1),
+                  _id = _ref5[0];
+
+              return id === _id;
+            })(stack);
+            if (method === 'close') {
+              stack[index][1].resolve();
+            } else if (method === 'dismiss') {
+              stack[index][1].reject();
+            }
+            delete stack[index];
+            _this2.$forceUpdate();
+          };
         };
-        modals.on('open', onOpen);
-        var onDestroy = function onDestroy(id) {
-          Vue.delete(_this2.modals, id);
+        var onKeydown = function onKeydown(event) {
+          if (event.keyCode == 27) {
+            // eslint-disable-line eqeqeq
+            var id = last(stack)[0];
+            modals.emit('dismiss', id);
+          }
         };
-        modals.on('destroy', onDestroy);
+        modals.on('open', function (event) {
+          return stack.push([event.id, event]);
+        });
+        modals.on('close', onDestroy('close'));
+        modals.on('dismiss', onDestroy('dismiss'));
+        $(document).on('keydown', onKeydown);
         this._unsubscribe = function () {
-          modals.off('open', onOpen);
-          modals.off('destroy', onDestroy);
+          modals.off('open');
+          modals.off('close');
+          modals.off('dismiss');
+          $(document).off('keydown', onKeydown);
         };
       },
       beforeDestroy: function beforeDestroy() {
@@ -97,17 +181,22 @@ export default {
       }
     });
     /**
-     * @param {Object} options
-     * @param {Object} options.data - data to pass into the modal instance
-     * @param {Function} options.modal - async require
+     * @param {object} options
+     * @param {object} options.data - data to pass into the modal instance
+     * @param {function} options.modal - async require
+     * @param {string} options.title - modal title
      */
-    Vue.prototype.$openModal = function (_ref2) {
-      var _ref2$data = _ref2.data,
-          data = _ref2$data === undefined ? {} : _ref2$data,
-          modal = _ref2.modal;
+    Vue.prototype.$openModal = function (_ref6) {
+      var _ref6$data = _ref6.data,
+          data = _ref6$data === undefined ? {} : _ref6$data,
+          modal = _ref6.modal;
 
-      modal(function (Modal) {
-        modals.emit('open', { Modal: Modal, data: data, id: hash({ Modal: Modal, data: data }) });
+      return Q.Promise(function (resolve, reject, notify) {
+        modal(function (Modal) {
+          var id = hash({ Modal: Modal, data: data });
+          notify(id);
+          modals.emit('open', { id: id, Modal: Modal, data: data, resolve: resolve, reject: reject });
+        });
       });
     };
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,82 @@
 import map from 'lodash/fp/map'
+import keyBy from 'lodash/fp/keyBy'
+import last from 'lodash/fp/last'
 import hash from 'object-hash'
+import findIndex from 'lodash/fp/findIndex'
 import values from 'lodash/fp/values'
 import EventEmmitter from 'events'
 import $ from 'jquery'
+import Q from 'q'
 
 export default {
   install (Vue, options) {
     const modals = new EventEmmitter()
+    let stack = []
     /**
      * style this component! use `.modal`, `.modal-container`, and `.modal-wrapper`
      * @function
      */
-    const ModalWrapper = Vue.component('modal-wrapper', { // eslint-disable-line no-unused-vars
+    const ModalWrapper = Vue.component('modal-wrapper', {
       render (h) {
-        return h('div', {class: {modal: true}}, [
-          h('div', {class: {'modal-container': true}}, [
-            h('div', {class: {'modal-wrapper': true}}, [
-              this.$slots.default
+        return h('div', {
+          class: {
+            modal: true
+          }
+        }, [
+          h('div', {
+            class: {
+              'modal-dialog': true
+            }
+          }, [
+            h('div', {
+              class: {
+                'modal-content': true
+              }
+            }, [
+              h('div', {
+                class: {
+                  'modal-header': true
+                }
+              }, [
+                h('button', {
+                  class: {'close': true},
+                  attrs: {
+                    'type': 'button',
+                    'aria-label': 'Close'
+                  }
+                }, [
+                  h('span', {
+                    attrs: {
+                      'aria-hidden': true
+                    },
+                    on: {
+                      click: this.dismiss()
+                    }
+                  }, '×')
+                ]),
+                h('h4', {class: {'modal-title': true}}, this.title)
+              ]),
+              h('div', {class: {'modal-body': true}}, [this.$slots.default]),
+              h('div', {class: {'modal-footer': true}}, [
+                h('button', {
+                  class: {
+                    'btn': true,
+                    'btn-default': true
+                  },
+                  on: {
+                    click: this.dismiss()
+                  }
+                }, 'Close'),
+                h('button', {
+                  class: {
+                    'btn': true,
+                    'btn-primary': true
+                  },
+                  on: {
+                    click: this.close()
+                  }
+                }, 'OK')
+              ])
             ])
           ])
         ])
@@ -25,30 +85,30 @@ export default {
         id: {
           type: String,
           required: true
+        },
+        title: {
+          type: String,
+          required: true
         }
       },
       methods: {
         close () {
-          modals.emit('destroy', this.id)
+          modals.emit('close', this.id)
+        },
+        dismiss () {
+          modals.emit('dismiss', this.id)
         }
       },
       mounted () {
         const input = $(this.$el).find('.modal-wrapper').first()
         const onClick = (event) => {
-          if (!input.is(event.target) && input.has(event.target).length === 0 && this.showDropdown) {
+          if (!input.is(event.target) && input.has(event.target).length === 0) {
             this.close()
           }
         }
         $(document).on('click', onClick)
-        const onKeydown = (event) => {
-          if (this.showDropdown && event.keyCode === 27) {
-            this.close()
-          }
-        }
-        $(document).on('keydown', onKeydown)
         this._unsubscribe = () => {
           $(document).off('click', onClick)
-          $(document).off('keydown', onKeydown)
         }
       },
       beforeDestroy () {
@@ -61,31 +121,48 @@ export default {
      */
     Vue.component('modal-view', {
       render (h) {
-        const modals = map(({Modal, data, id}) => h(ModalWrapper, {
-          attrs: {id}
+        const modals = map(({Modal, data, id, title}) => h(ModalWrapper, {
+          attrs: {id},
+          props: {title}
         }, [
           h(Modal, {props: data})
         ]))
         return h('div', null, modals(values(this.modals)))
       },
-      data () {
-        return {modals: {}}
+      computed: {
+        modals () {
+          return keyBy(([id]) => id)(stack)
+        }
       },
       /**
        * register listeners to add/remove Modals and corresponding data
        */
       beforeCreate () {
-        const onOpen = (event) => {
-          Vue.set(this.modals, event.id, event)
+        const onDestroy = (method) => (id) => {
+          const index = findIndex(([_id]) => id === _id)(stack)
+          if (method === 'close') {
+            stack[index][1].resolve()
+          } else if (method === 'dismiss') {
+            stack[index][1].reject()
+          }
+          delete stack[index]
+          this.$forceUpdate()
         }
-        modals.on('open', onOpen)
-        const onDestroy = (id) => {
-          Vue.delete(this.modals, id)
+        const onKeydown = (event) => {
+          if (event.keyCode == 27) { // eslint-disable-line eqeqeq
+            const id = last(stack)[0]
+            modals.emit('dismiss', id)
+          }
         }
-        modals.on('destroy', onDestroy)
+        modals.on('open', (event) => stack.push([event.id, event]))
+        modals.on('close', onDestroy('close'))
+        modals.on('dismiss', onDestroy('dismiss'))
+        $(document).on('keydown', onKeydown)
         this._unsubscribe = () => {
-          modals.off('open', onOpen)
-          modals.off('destroy', onDestroy)
+          modals.off('open')
+          modals.off('close')
+          modals.off('dismiss')
+          $(document).off('keydown', onKeydown)
         }
       },
       beforeDestroy () {
@@ -93,13 +170,18 @@ export default {
       }
     })
     /**
-     * @param {Object} options
-     * @param {Object} options.data - data to pass into the modal instance
-     * @param {Function} options.modal - async require
+     * @param {object} options
+     * @param {object} options.data - data to pass into the modal instance
+     * @param {function} options.modal - async require
+     * @param {string} options.title - modal title
      */
     Vue.prototype.$openModal = function ({data = {}, modal}) {
-      modal((Modal) => {
-        modals.emit('open', {Modal, data, id: hash({Modal, data})})
+      return Q.Promise((resolve, reject, notify) => {
+        modal((Modal) => {
+          const id = hash({Modal, data})
+          notify(id)
+          modals.emit('open', {id, Modal, data, resolve, reject})
+        })
       })
     }
   }
